27a9f033c2181bf6fb81780cb102151c248be1c8
npedrini <go@looklisten.net>
2014-05-06 13:01:57 -0400

    simplified grid resize

--- www/app/directive/resizable.js
+++ www/app/directive/resizable.js
@@ -15,37 +15,79 @@ app.directive
 					{
 						var constrain = scope.componentDefinition.resizable && (typeof scope.componentDefinition.resizable == 'object') && scope.componentDefinition.resizable.constrain;
 						
-						angular.element(element).resizable
-						(
+						var options = 
+						{
+							ghost:true,
+							autoHide:true,
+							aspectRatio:constrain,
+							stop:function(event,ui)
 							{
-								ghost:true,
-								autoHide:true,
-								aspectRatio:constrain,
-								stop:function(event,ui)
-								{
-									var model = $parse(attrs.resizable);
-									
-									scope.$apply
-									(
-										function()
+								var cellData = null;
+								
+								//	look for cell data
+								//	this allows us to resize cells in the same row/column when resizing horizontally/vertically for a cell-based element
+								if( attrs.resizableCell )
+									cellData = scope.$eval(attrs.resizableCell);
+								
+								scope.$apply
+								(
+									function()
+									{
+										var model = $parse(attrs.resizable);
+										var vals = model(scope);
+										
+										if( !vals ) vals = {};
+										
+										if( cellData )
 										{
-											var model = $parse(attrs.resizable);
-											var vals = model(scope);
+											//	vertical resize
+											//	resize width for all cells in the corresponding column
+											if ( ui.originalSize.width !== ui.size.width ) 
+											{
+												for(var i=0,col = cellData.index%cellData.cols;i<cellData.cols*cellData.rows;i++)
+												{
+													if(col == i%cellData.cols)
+													{
+														if( !vals[i] ) vals[i] = {};
+										        		
+										        		vals[i].width = ui.size.width;
+													}
+												}
+									        }
 											
-											if( !vals )
-												vals = {};
-											
+											//	horizontal resize
+											//	resize height for all cells in the corresponding row
+									        if ( ui.originalSize.height !== ui.size.heigh ) 
+									        {
+									        	var start = Math.floor( cellData.index/cellData.cols ) * cellData.cols;
+									        	var end = start + (cellData.cols - 1);
+									        	
+									        	for(var i=start;i<=end;i++)
+									        	{
+									        		if( !vals[i] ) vals[i] = {};
+									        		
+									        		vals[i].height = ui.size.height;
+									        	}
+									        }
+										}
+										else
+										{
 											vals.width = ui.size.width;
 											vals.height = ui.size.height;
-											
-											model.assign(scope,vals);
 										}
-									);									
-									
-									HistoryService.save( "Resized to " + scope.definition.values.width + " x " + scope.definition.values.height );
-								}
+										
+										model.assign(scope,vals);
+									}
+								);									
+								
+								HistoryService.save( "Resized to " + scope.definition.values.width + " x " + scope.definition.values.height );
 							}
-						);
+						};
+						
+						if( attrs.resizableConfig )
+							options = _.defaults(scope.$eval(attrs.resizableConfig),options);
+						
+						angular.element(element).resizable(options);
 					}
 					else
 					{
