4fca8dcdf1ea9f7368b4fcef10752bd33ebf6af4
npedrini <go@looklisten.net>
2014-06-16 13:59:55 -0400

    reworked confirm dialogs

--- www/app/directive/confirm-dialog.js
+++ www/app/directive/confirm-dialog.js
@@ -16,18 +16,24 @@ app.directive
 						function() 
 						{
 							var message = attrs.confirmDialogMessage;
+							var title = attrs.confirmDialogTitle || "Edit";
+							var noButtonText = attrs.confirmDialogNoButton || "No";
+							var yesButtonText = attrs.confirmDialogYesButton || "Yes";
 							
-							var header = '<div class="modal-header"><button type="button" class="close" ng-click="cancel()" aria-hidden="true">&times;</button><h4 class="modal-title">Edit</h4></div>';
-							var body = '<div class="modal-body">{{message}}</div>';
-							var footer = '<div class="modal-footer"><button type="button" class="btn btn-default" ng-click="cancel()">No</button><button type="button" class="btn btn-primary" ng-click="save()">Yes</button></div>';
+							var header = '<div class="modal-header"><button type="button" class="close" ng-click="cancel()" aria-hidden="true">&times;</button><h4 class="modal-title">{{title}}</h4></div>';
+							var body = '<div class="modal-body" style="text-align:center">{{message}}</div>';
+							var footer = '<div class="modal-footer"><button type="button" class="btn btn-default" ng-click="cancel()">{{noButtonText}}</button><button type="button" class="btn btn-primary" ng-click="confirm()">{{yesButtonText}}</button></div>';
 							
-							var ModalCtrl = function($scope,$modalInstance,message,callback)
+							var ModalCtrl = function($scope,$modalInstance,callback,args)
 						    {
 								$scope.message = message;
+								$scope.title = title;
+								$scope.noButtonText = noButtonText;
+								$scope.yesButtonText = yesButtonText;
 								
-								$scope.save = function()
+								$scope.confirm = function()
 								{
-									$parse(callback)(scope)();
+									$parse(callback)(scope).apply(args);
 									
 						       		$modalInstance.close();
 						      	};
@@ -44,8 +50,12 @@ app.directive
 					    			template: header + body + footer,
 					    			controller: ModalCtrl,
 					    			resolve: {
+					    				callback: function(){ return attrs.confirmDialogCallback; },
+					    				args: function(){ return attrs.confirmDialogCallbackArgs; },
 					    				message: function(){ return message; },
-					    				callback: function(){ return attrs.confirmDialogCallback; }
+					    				noButtonText: function(){ return noButtonText; },
+					    				title: function(){ return title; },
+					    				yesButtonText: function(){ return yesButtonText; }
 									}
 					    		}
 						    );
