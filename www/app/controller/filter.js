7b61f8ed7488ee55c799c878af40c28e260f0291
npedrini <go@looklisten.net>
2014-06-24 12:47:55 -0400

    persist filter across sessions

--- www/app/controller/filter.js
+++ www/app/controller/filter.js
@@ -2,17 +2,24 @@ app.controller
 (
 	'FilterCtrl',
 	[
-	 	'$scope',
-	 	function($scope)
+	 	'$scope','webStorage',
+	 	function($scope,webStorage)
 	 	{
 	 		$scope.filters = [
 	 		                  {value:'content.name',label:'Alphabetical'},
 	 		                  {value:'created',label:'Recently Created'},
 	 		                  {value:'updated',label:'Recently Modified'}
 	 		                 ];
-	 		$scope.filter = $scope.filters[0].value;
+	 		$scope.filter = webStorage.get('filter') || $scope.filters[0].value;
 	 		
 	 		$scope.search = null;
+	 		
+	 		$scope.setFilter = function(filter)
+	 		{
+	 			$scope.filter = filter;
+	 			
+	 			webStorage.add('filter',filter);
+	 		};
 	 		
 	 		$scope.filterByTitle = function(item)
 	 		{
